39c6ffad0f9f8df72a0b1612de0e29e1deb9a8f7
yamamotorinpei <49706282+yamamotorinpei@users.noreply.github.com>
2019-07-17 14:08:54 +0900

    Create server1.js

---
+++ server1.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const server = express();
+const ejs = require('ejs');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'node',
+    password: 'websystem',
+    database: 'web'
+});
+
+server.get('/', function( req, res ) {
+    connection.query('select id, 都道府県, 人口 from example order by 人口 desc limit 10;', (error, rows, fields) => {
+        if( error ) {
+            console.log('Query Error');
+        }
+        res.render( 'sql.ejs', { content: rows });
+    });
+});
+
+server.listen( 80, function() {
+    console.log( 'listening on port 80' );
+});
