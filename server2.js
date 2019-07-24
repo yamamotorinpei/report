43447989af49010e9b5fb24dcbf651bf80a9b88c
yamamotorinpei <49706282+yamamotorinpei@users.noreply.github.com>
2019-07-24 15:52:07 +0900

    Create server2.js

---
+++ server2.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const server = express();
+const ejs = require('ejs');
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'node',
+    password: 'websystem',
+    database: 'web'
+});
+
+server.get('/', function( req, res ) {
+    let sorting = req.query.sorting || 'WR';
+    let number = req.query.number || 10;
+    let query = 'select id, yaer, ' + sorting + ' from example order by ' + sorting + ' desc limit ' + number + ';';
+    console.log( query );
+    connection.query( query, (error, rows, fields) => {
+        if( error ) {
+            console.log('Query Error');
+        }
+        res.render( 'sql2.ejs', { content: rows });
+    });
+});
+
+server.listen( 80, function() {
+    console.log( 'listening on port 80' );
+});
