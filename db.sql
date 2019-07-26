faa8ee9ef5bd2a52f4f24078271bb14c92f6c2fb
yamamotorinpei <49706282+yamamotorinpei@users.noreply.github.com>
2019-07-26 16:08:05 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-create table player ( id int auto_increment not null primary key, name varchar(100) );
-create table team ( id int auto_increment not null primary key, name varchar(100) );
+create table player ( id int auto_increment not null primary key, player_name varchar(100) );
+create table team ( id int auto_increment not null primary key, team_name varchar(100) );
 create table fight ( id int auto_increment not null primary key, year int, player_id int, team_id int, OV int, TV int, WR int );
 load data local infile 'player.csv' into table player fields terminated by ',' enclosed by '"';
 load data local infile 'team.csv' into table team fields terminated by ',' enclosed by '"';
